d95fa4e51c9550c6255811ed128ea14278c8bca8
Clydeside <Clydeside@github.com>
2023-05-02 23:10:09 +0300

    Adding 16th

--- twenty/16.cpp
+++ twenty/16.cpp
@@ -5,10 +5,12 @@ using namespace std;
 int main() {
     int n1 = 1, n2 = 2;
     int n = 5;
+    int prev = 1;
     unsigned long long fib = 1;
     for (int i = 1; i < n + 1; ++i) {
-        int prev = 1;
-        fib += prev + i;
+        int cur = fib;
+        fib = prev + cur;
+        prev = cur;
     }
     cout << "Fib sequence: " << fib << "\n";
 
